bfddc9a4cc5531767e77b6829bf180a637a783cc
Kiwoong Kim <gw150311@naver.com>
2024-03-01 23:15:28 +0900

    upload fruits images

--- main.js
+++ main.js
@@ -1,24 +1,66 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { Bodies, Engine, Render, Runner, World } from "matter-js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+/* Constans */
+const background_color = '#F7F4C8';
+const wall_color = '#E6B143';
 
-setupCounter(document.querySelector('#counter'))
+const engine = Engine.create();
+const world = engine.world;
+
+const [board_width, board_height] = [620, 850];
+const render = Render.create({
+  engine,
+  element: document.body,
+  options: {
+    wireframes: false,
+    background: background_color,
+    width: board_width,
+    height: board_height,
+  }
+});
+
+const [wall_width, wall_heignt] = [30, 790];
+const center_left_wall = [wall_width / 2, wall_heignt / 2];
+const center_right_wall = [board_width - wall_width / 2, wall_heignt / 2];
+
+const leftWall = Bodies.rectangle(
+  center_left_wall[0], center_left_wall[1],
+  wall_width, wall_heignt,
+  {
+    isStatic: true,
+    render: { fillStyle: wall_color }
+  });
+
+const rightWall = Bodies.rectangle(
+  center_right_wall[0], center_right_wall[1],
+  wall_width, wall_heignt,
+  {
+    isStatic: true,
+    render: { fillStyle: wall_color }
+  });
+
+const groud_height = board_height - wall_heignt;
+const center_ground = [board_width / 2, board_height - groud_height / 2]
+
+const ground = Bodies.rectangle(
+  center_ground[0], center_ground[1],
+  board_width, groud_height,
+  {
+    isStatic: true,
+    render: { fillStyle: wall_color }
+  });
+
+const top_line_thick = 2;
+const topLine = Bodies.rectangle(
+  board_width / 2, board_height / 10,
+  board_width, top_line_thick,
+  {
+    isStatic: true,
+    render: { fillStyle: wall_color }
+  }
+);
+
+World.add(world, [leftWall, rightWall, ground, topLine]);
+
+Render.run(render);
+Runner.run(engine);
